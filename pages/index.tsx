e28a5706e01a7de472bc7538b399ee15cce4d18d
Brendan Hemat-Siraky <brendansirakydeveloper@gmail.com>
2024-03-20 21:44:38 +0000

    Test update import

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,27 +3,27 @@ import Head from 'next/head'
 
 import { Hero, Me, Projects, Archives } from '../components/landing'
 import { LandingContext } from '../model/context/LandingContext'
-import { Layout } from '../components/shared/layout/Layout'
+// import { Layout } from '../components/shared/layout/Layout'
 import { landing } from '../content/landing'
 
 const Landing: NextPage = () => {
 	return (
 		<LandingContext.Provider value={landing}>
-			<Layout>
-				<Head>
-					<title>Brendan Hemat-Siraky | Portfoilio</title>
-					<meta
-						name="description"
-						content="Brendan is a Full Stack developer that specialises in building Mobile and Web Applications"
-					/>
-					<link rel="icon" href="/favicon.ico" />
-				</Head>
+			{/* <Layout> */}
+			<Head>
+				<title>Brendan Hemat-Siraky | Portfoilio</title>
+				<meta
+					name="description"
+					content="Brendan is a Full Stack developer that specialises in building Mobile and Web Applications"
+				/>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 
-				<Hero />
-				<Me />
-				<Projects />
-				<Archives />
-			</Layout>
+			<Hero />
+			<Me />
+			<Projects />
+			<Archives />
+			{/* </Layout> */}
 		</LandingContext.Provider>
 	)
 }
